26b85430a8370ef435f654cf8bb3169bc9fdfb73
lohitha <atluri.lo@northeastern.edu>
2024-09-27 15:51:25 -0400

    labmodule2

--- src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
+++ src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
@@ -13,7 +13,7 @@ package programmingtheiot.gda.app;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import programmingtheiot.gda.system.SystemPerformanceManager;
 /**
  * Main GDA application.
  * 
@@ -28,7 +28,7 @@ public class GatewayDeviceApp
 	public static final long DEFAULT_TEST_RUNTIME = 60000L;
 	
 	// private var's
-	
+	private SystemPerformanceManager sysPerfMgr = null;
 	
 	// constructors
 	
@@ -42,7 +42,7 @@ public class GatewayDeviceApp
 		super();
 		
 		_Logger.info("Initializing GDA...");
-		
+		this.sysPerfMgr = new SystemPerformanceManager();
 		parseArgs(args);
 	}
 	
@@ -61,7 +61,7 @@ public class GatewayDeviceApp
 		gwApp.startApp();
 		
 		try {
-			Thread.sleep(DEFAULT_TEST_RUNTIME);
+			Thread.sleep(65000L);
 		} catch (InterruptedException e) {
 			// ignore
 		}
@@ -82,8 +82,13 @@ public class GatewayDeviceApp
 		
 		try {
 			// TODO: Your code here
-			
-			_Logger.info("GDA started successfully.");
+			if (this.sysPerfMgr.startManager()) {
+				_Logger.info("GDA started successfully.");
+			} else {
+				_Logger.warning("Failed to start system performance manager!");
+				
+				stopApp(-1);
+			}
 		} catch (Exception e) {
 			_Logger.log(Level.SEVERE, "Failed to start GDA. Exiting.", e);
 			
@@ -101,9 +106,11 @@ public class GatewayDeviceApp
 		_Logger.info("Stopping GDA...");
 		
 		try {
-			// TODO: Your code here
-			
-			_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
+			if (this.sysPerfMgr.stopManager()) {
+				_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
+			} else {
+				_Logger.warning("Failed to stop system performance manager!");
+			}
 		} catch (Exception e) {
 			_Logger.log(Level.SEVERE, "Failed to cleanly stop GDA. Exiting.", e);
 		}
